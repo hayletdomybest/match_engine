26e92d4f3851427496c6a9c3fbf5f6cf9e9146a0
James <james@Jamess-MacBook-Pro.local>
2024-06-16 06:55:21 +0800

    update match engine match logic

--- domain/engine/match_engine_service.go
+++ domain/engine/match_engine_service.go
@@ -1,22 +1,49 @@
 package engine
 
 import (
+	"match_engine/domain"
 	mk "match_engine/domain/market"
 	odr "match_engine/domain/order"
+	"match_engine/utils"
 )
 
 type MatchEngine struct {
 	orderRepo  odr.OrderRepository
 	marketRepo mk.MarketRepository
+
+	eventDispatcher domain.EventDispatcher
 }
 
-func NewMatchEngin(orderRepo odr.OrderRepository, marketRepo mk.MarketRepository) *MatchEngine {
+func NewMatchEngin(
+	orderRepo odr.OrderRepository,
+	marketRepo mk.MarketRepository,
+	eventDispatcher domain.EventDispatcher) *MatchEngine {
 	return &MatchEngine{
-		orderRepo:  orderRepo,
-		marketRepo: marketRepo,
+		orderRepo:       orderRepo,
+		marketRepo:      marketRepo,
+		eventDispatcher: eventDispatcher,
 	}
 }
 
-func Match(order *odr.Order) {
+func (me *MatchEngine) Match(order *odr.Order) MatchResult {
 	//TODO
+	var matched []odr.Order
+	var result MatchResult
+	for !order.IsDone() {
+		matchedOrder, _ := me.orderRepo.FetchMatchingOrders(*order, 1)
+		// more logic
+		if len(matchedOrder) > 0 {
+			matched = append(matched, matchedOrder...)
+		}
+
+		aggregate := odr.NewOrderAggregate(order)
+		aggregate.Match(utils.Select(matchedOrder, func(o odr.Order) *odr.Order { return &o }))
+	}
+
+	me.orderRepo.Save(append(matched, *order)...)
+
+	//TODO
+	//me.eventDispatcher.Dispatch()
+
+	return result
 }
